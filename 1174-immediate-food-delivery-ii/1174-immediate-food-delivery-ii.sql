53b0ec4926572d37046971e3ccb26d728aecab59
Ted <hlgevan5@liverpool.ac.uk>
2025-12-17 14:33:27 +0000

    Time: 527 ms (97.96%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100, 2) AS immediate_percentage
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+    FROM Delivery
+) t
+WHERE rn = 1;
